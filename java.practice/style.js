f6d102756e0034e91d691594fe9a5b1f08916d99
cemile-2004 <cemile.meherremova@gmail.com>
2024-10-31 19:36:49 +0400

    done

---
+++ java.practice/style.js
@@ -0,0 +1,371 @@
+// let age = +prompt("enter age");
+// if (age > 0 && age < 125) {
+//   if (age >= 65) {
+//     console.log("pensiya yasina catmisiz");
+//   } else {
+//     console.log("pensiya yasina catmamisiniz");
+//   }
+// }
+
+//  let num1= +prompt("1 ci reqemi daxil edin");
+//  let num2= +prompt("2 ci reqemi daxil edin");
+//  let num3= +prompt("3 ci reqemi daxil edin");
+
+//  if(num1>0 && num2>0 && num3>0){
+//     if(num1===num2 && num2!==num3 & num1===num3){
+//         console.log("Ucbucaq beraber yanlidir")
+//     }
+//     if(num1===num2 && num2===num3){
+//         console.log("ucbucaq berabertereflidir")
+//     }
+//     else{
+//         console.log("ucbucaq muxtelif tereflidir")
+//     }
+//  }
+
+// let eded = +prompt("eded daxil edin")
+// if(eded % 2==0){
+// console.log("eded cut ededdir")
+
+// }
+// else if(eded==0){
+//     console.log("ne tek ne cut")
+// }
+// else{
+//     console.log("eded tekdir")
+// }
+
+// let day = +prompt(" 1-12 arasi reqemi daxil edin")
+// switch(day){
+// case 1:
+//     console.log("Yanvar ayi")
+//     break;
+// case 2:
+//     console.log("Fevral ayi")
+//     break;
+// case 3:
+//     console.log("Mart ayi")
+//     break;
+// case 4:
+//     console.log("Aprel ayi")
+//     break;
+// case 5:
+//     console.log("May ayi")
+//     break;
+// case 6:
+//     console.log("Iyun ayi")
+//     break;
+// case 7:
+//     console.log("Iyul ayi")
+//     break;
+// case 8:
+//     console.log("Avqust ayi")
+//     break;
+// case 9:
+//     console.log("Sentyabr ayi")
+//     break;
+// case 10:
+//     console.log("Oktyabr ayi")
+//     break;
+// case 11:
+//     console.log("Noyabr ayi")
+//     break;
+// case 12:
+//     console.log("Dekabr ayi")
+//     break;
+
+//     default:
+//         console.log("bele bir ay yoxdur")
+// }
+
+// let reqem = +prompt("reqem daxil edin")
+// if(eded>0){
+//     if(reqem%3==0 && reqem%5==0){
+//         console.log("eded hem 3 hem 5 e bolunur")
+//     }
+//  else{
+//     console.log("eded 3 ve 5 e bolunmur")
+//  }
+
+// }
+
+// let eded = +prompt("eded daxil edin")
+
+// let result;
+// if(eded%2==0){
+//     console.log("bolundu")
+// }
+// else{
+//     console.log(result)
+// }
+
+// let eded1 = +prompt("eded daxil edin")
+// let eded2= +prompt ("eded daxil edin")
+// if(eded1*eded2>0){
+//     console.log("musbetdir")
+// }
+// else{
+//     console.log("menfidir")
+// }
+
+// let eded = +prompt("eded daxil edin")
+// if(eded>=1 && eded<=100){
+//     console.log("eded 1-100 arsindadir")
+
+// }
+// else{
+//     console.log("1-100 arasinda deyil")
+
+// }
+
+// let eded1 = +prompt("eded daxil edin")
+// let eded2 = +prompt("eded daxil edin")
+// let eded3 = +prompt("eded daxil edin")
+// max=max(eded1,eded2,eded3)
+// console.log(max)
+
+// let bal= +prompt("bal daxil edin")
+// if(bal>0){
+//     if(bal>=90 && bal<=100){
+//         console.log("A")
+
+//     }
+//     else if(bal>=80 && bal<90 ){
+//         console.log("B")
+//     }
+//     else if(bal>=70 && bal<80 ){
+//         console.log("C")
+//     }
+//     else if(bal>=60 && bal<70 ){
+//         console.log("D")
+//     }
+//     else if( bal<=59 ){
+//         console.log("F")
+//     }
+
+// }
+// else{
+//     console.log("menfi eded daxil etmek olmaz")
+// }
+
+// for (let index = 1; index < 101; index++) {
+//    if(index%2==0){
+//     console.log(index);
+
+//    }
+
+// }
+
+// let count = 0;
+// for (let index = 1; index < 101; index++) {
+//   if (index % 2 == 1) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// let sum = 0;
+// for (let index = 1; index < 101; index++) {
+//   if (index % 5 == 0) {
+//     sum += index;
+//   }
+// }
+// console.log(sum);
+
+// let arr=[12,14,35,56,68,93];
+// let sum=0
+// for (let index = 0; index < arr.length; index++) {
+//    sum+=arr[index];
+
+// }
+// console.log(sum);
+
+// let arr = [12, 14, 35, 56, 68, 93];
+// for (let index = 1; index < arr.length; index++) {
+//   if (index % 2 == 1) {
+//     console.log(arr[index]);
+//   }
+// }
+
+// let arr=[12,14,35,56,68,93];
+// for (let index = 0; index < arr.length; index++) {
+//  if(index%2==0){
+// console.log(arr[index]);
+//  }
+
+//  }
+
+// let eded =+prompt("eded daxil edin");
+// let factorial=1
+// for (let index = 1; index <= eded; index++) {
+//    factorial*=index
+// }
+// alert(factorial)
+
+// let str="lorem ipsum dolar sit amet consectetur adipisicing elit.Voluptatibus ,prasentium.";
+// let count=0
+// for (let index = 0; index < str.length; index++) {
+//     if(str[index]==="L"){
+//     count++
+//     }
+
+// }
+// console.log(count)
+
+// const countries=["azerbaijan","norwey","albania","germany","america"];
+// for (let index = 0; index <  countries.length; index++) {
+//     if(countries[index][0]===a)
+//         console.log(countries[index])
+
+// }
+
+// const scores=[60,75,88,75,93,45]
+//  let sum=0;
+// for (let index = 0; index < scores.length; index++) {
+//     sum+=scores[index];
+
+// } console.log(sum/scores.length);
+
+// 1.Verilmiş arraydə cüt ədədlərin ən böyüynü tapan alqoritm yazin.
+
+// let array = [12,23,43,54,65,67,89,90,100]
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]%2===0){
+//         console.log(Math.max(array[index]))
+//     }
+
+// }
+
+// 2.Verilmiş arraydə tək indexdə duran ən böyük tək ədədi tapan alqoritm yazin.
+
+// let array = [12,23,43,54,65,67,89,90,100]
+// for (let index = 0; index < array.length; index++) {
+//     if(array%2===1  && array[index]%2===1){
+
+//     }
+
+// }console.log(Math.max(array[index]))
+
+// 3.Verilmiş arraydə ən böyük və ən kiçik ədədlərini hasili ilə cəmini müqasiyə edin.
+
+// let array = [12,23,43,54,65,67,89,90,100]
+// for (let index = 0; index < array.length; index++) {
+
+//  console.log(Math.max(array[index]))
+//  console.log(Math.min(array[index]))
+
+//  let cem=0
+//  let hasil=1
+//  cem=Math.max(array[index])+Math.min(array[index])
+//  hasil=Math.max(array[index])*Math.min(array[index])
+//  if(cem>hasil){
+//     console.log($[cem]>$[hasil])
+//  }
+//  else{
+//     console.log($[hasil]>$[cem])
+//  }
+
+// }
+
+// 4.Verilmis ədədin reqemlerinin cemini tapan alqoritm yazin.
+
+// 5.Verilmiş arraydə ən böyük ədədlə ən kiçik ədədin yerini dəyişin
+// let array = [12,23,43,54,65,67,89,90,100]
+// for (let index = 0; index < array.length; index++) {
+
+//     console.log(Math.max(array[index]))
+//     console.log(Math.min(array[index]))
+// }
+
+// Verilmiş arraydə təkrərlanan elementi tapin.
+// let array = [12,23,43,54,65,67,89,90,100]
+
+// for (let index = 0; index < array.length; index++) {
+//     let element=[0]
+//     if(element===element[index]) {
+//         console.log($[element[index]])
+//     }
+
+// }
+
+//  7 Bu array-dəki insanların orta yaşını hesablayın.
+
+// let people = [
+//   { name: "Aysel", age: 25 },
+//   { name: "Rəşad", age: 30 },
+//   { name: "Leyla", age: 18 },
+//   { name: "Kamal", age: 40 },
+// ];
+// let sum = 0;
+// for (let index = 0; index < people.length; index++) {
+ 
+//   sum += people[index].age;
+// }
+// console.log(sum / 4);
+
+
+// city dəyəri Bakı olan və yaşı 20-dən böyük olan insanları tapıb adlarını çıxarın.
+// let people = [
+//     { name: "Aysel", age: 25, city: "Baki" },
+//     { name: "Rəşad", age: 30, city: "Gəncə" },
+//     { name: "Leyla", age: 18, city: "Baki" },
+//     { name: "Kamal", age: 40, city: "Sumqayıt" }
+// ];
+// for (let index = 0; index < people.length; index++) {
+//     if(people[index].city=="Baki"  && people[index].age>20){
+//         console.log(people[index].name)
+//     }
+        
+    
+// }
+
+// 10. İki Array-də Eyni Elementlərin Tapılması
+
+// let array1 = [1, 3, 5, 7];
+// let array2 = [2, 3, 6, 7, 8];
+//  for (let index = 0; index < array1.length; index++) {
+//     for (let j = 0; index < array2.length; j++) {
+//         if(array1[index]==array[j]){
+//             console.log(array1[index])
+//         }
+
+// 11.Verilmiş arraydə mənfi və müsbət ədədlərin sayini tapin.
+// let array =[-10,12,-23,40,60,73,-6,-20];
+// let menfiSay=0;
+// let musbetSay=0;
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]>0){ 
+
+//        musbetSay++;
+//      if(array[index<0])   {
+//        menfiSay++;
+//      }
+//     }
+    
+// }console.log(menfiSay,musbetSay)
+        
+
+// 12.Verilmiş arraydə mənfi ədədlərin hasili ilə müsbət ədədlərin hasilini müqasiyə edin. 
+//    let array =[-10,12,-23,40,60,73,-6,-20];
+//    let menfihasil=1;
+//    let musbethasil=1;
+//    for (let index = 0; index < array.length; index++) {
+//      if(array[index]>0){
+//         musbethasil*=array[index]
+
+//      }
+//      else if(array[index]<0){
+//         menfihasil*=array[index]
+//      }
+    
+//    }console.log(menfihasil)
+//    console.log(musbethasil)
+
+// 13.Verilmiş arrayi tərsinə çevirib yazin.
+// let array =[11,12,14,56]
+// let reserved;
+// for (let index = array.length; index >=0; index--) {
+//     reserved.push(array[index])
+   
+// } console.log(array[index]);
+
