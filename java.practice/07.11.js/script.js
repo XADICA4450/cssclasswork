a2e52e98d12b7a1c4371723a442f54a31c6f70d1
cemile-2004 <cemile.meherremova@gmail.com>
2024-11-07 18:57:39 +0400

    dom added

---
+++ java.practice/07.11.js/script.js
@@ -0,0 +1,21 @@
+
+
+const cardDiv = document.createElement("div");
+cardDiv.textContent="Azmp202";
+cardDiv.style.width="200px";
+cardDiv.style.height="260px";
+cardDiv.style.backgroundColor="green";
+cardDiv.style.border="2px solid red";
+cardDiv.style.borderRadius="15px";
+cardDiv.style.color="white";
+cardDiv.style.fontsize="20px";
+cardDiv.style.display="flex";
+cardDiv.style.justifyContent="center"
+document.body.append(cardDiv)
+
+
+document.body.style.display="flex";
+document.body.style.justifyContent="center";
+document.body.style.margin="0";
+document.body.style.alignItems='center';
+
